7f4659b28f79579bf86a7afdcd90a19848a343af
luma678 <l.marlow@wlv.ac.uk>
2025-02-24 17:46:35 +0000

    Initialise Nav Bar
    Added a nav bar along with some of the features that are part of the functional requirements for developer ease of creation.

--- code/footer.php
+++
@@ -1,4 +0,0 @@
-</div>
-</body>
-<footer>&copy; 2025 DEV DYNAMICS</footer>
-</html>
--- code/includes/dbh.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Connect to database
-$dbconnection = new mysqli("localhost","2350327","8g5sps","db2350327");
-if ($dbconnection -> connect_errno) {
-echo "Failed to connect to MySQL: " . $dbconnection -> connect_error;
-exit();
-}
