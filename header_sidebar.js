77caeee342d7ac1b1428f0b1a2ca89f8b9756b6d
jay <68795028+zwagk@users.noreply.github.com>
2025-03-17 18:26:24 +0000

    Responsive Header
    Fixed issues with header overlap on smaller dimensions by adding a sidebar using free google icons and implementing simple JS functions to show and hide the menu sidebar when device dimension is < 800px width.

---
+++ header_sidebar.js
@@ -0,0 +1,15 @@
+// Function to show the sidebar menu
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+
+    // Changes the display style to "flex", making the sidebar visible
+    sidebar.style.display = 'flex'
+}
+
+// Function to hide the sidebar menu
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+
+    // Changes the display style to "none", making the sidebar invisible
+    sidebar.style.display = 'none'
+}
